c271b247a83d829e1ff10db17f64c434909348f2
syk531 <syk531@pluxity.com>
2024-03-18 10:05:43 +0900

    react todo app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import React, { Component } from "react";
 import './App.css';
 
 export default class App extends Component {
+
+    state = {
+        todoData : [
+            {
+                id: "1",
+                title: "공부하기",
+                completed: true
+            },
+            {
+                id: "2",
+                title: "청소하기",
+                completed: false
+            }
+        ]
+    }
+
+    btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
+    }
+
+    getStyle = () => {
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: 'none'
+        }
+    }
+
+    handleClick = (id) => {
+        let newTodoData = this.state.todoData.filter(data => data.id != id);
+        console.log(newTodoData);
+        this.setState({todoData: newTodoData});
+    }
+
     render() {
         return (
             <div className="container">
@@ -10,11 +49,13 @@ export default class App extends Component {
                         <h1>할 일 목록</h1>
                     </div>
 
-                    <div>
-                        <input type="checkbox" defaultChecked={false} />
-                        공부하기
-                        <button>x</button>
-                    </div>
+                    {this.state.todoData.map(data => (
+                        <div style={this.getStyle()} key={data.id}>
+                            <input type="checkbox" defaultChecked={false}/>
+                                {data.title}
+                            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+                        </div>
+                    ))}
                 </div>
             </div>
         )
